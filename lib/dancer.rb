6ae7610da7c6a1b1ee696994a776ee9cb27ceef7
Vi Vo <aaronvo118@gmail.com>
2022-03-16 08:33:34 -0500

    commit tree 7fc536ca4a657d2dd3e3c74654ee820d206094e2

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,19 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative 'fancy_dance.rb'
+
+require 'pry'
+class Dancer
+    extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+  
+    attr_accessor :name
+  
+    def initialize(name)
+      @name = name
+    end
+  end
+
+  angelina = Dancer.new("angelina")
+
+  binding.pry
